494cddc014c2f5f87d40127e59d972cf0c444660
hanjunehee0 <hanjunehee0@acrofuture.com>
2025-04-21 14:59:25 +0900

    feat: #33-Loading-Component  - 로딩 컴포넌트 작업  - CSS 스타일 작업

---
+++ src/components/common/loading/loading.tsx
@@ -0,0 +1,8 @@
+export const LoadingComponent = () => {
+    return (
+        <div className="loading-wrap">
+            <div className="loading-spinner"></div>
+            <div className="loading-text">Loading...</div>
+        </div>
+    )
+}
